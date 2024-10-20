f85da8877b92467fd66ef6eb7d20b50fbf513e14
Kiyotoko <karl.zschiebsch@gmail.com>
2024-10-20 15:10:31 +0200

    Fix doors and plates

--- main.lua
+++ main.lua
@@ -1,4 +1,7 @@
--- the player
+-- Game created by Leo and Karl for the gamejam 2024.
+---@diagnostic disable: lowercase-global, undefined-global
+
+-- the player data
 player = {
 	x=-32, -- x position on the map in pixels
 	y=-32, -- y position on the map in pixels
@@ -9,37 +12,22 @@ player = {
 -- player on the screen
 ancor = {x=64,y=64}
 
--- markers that are checked
-checkmarks = 0
-
+-- the doors on the map
 doors = {
-	{
-		{x=8,y=3},
-		{x=8,y=4},
-		{x=8,y=5}
-	},
-	{
-		{x=13,y=0},
-		{x=13,y=1},
-		{x=13,y=2},
-		{x=13,y=3},
-		{x=13,y=4}
-	},
-	{
-		{x=13,y=6},
-		{x=13,y=7},
-		{x=13,y=8},
-		{x=13,y=9},
-		{x=13,y=10},
-		{x=13,y=11}
-	}
+	[0] = {x=8,y=3,d=1},
+	[1] = {x=13,y=0,d=1},
+	[2] = {x=13,y=6,d=1}
 }
+
+-- defined flags
+flag_free = 0
+flag_door = 1
 
 -- returs the sprite based on the position of the player
 -- please note that the position is in pixels
 function get_sprite(x,y)
-	player_column = (x+ancor.x) / 8
-	player_row = (y+ancor.y) / 8
+	local player_column = (x+ancor.x) / 8
+	local player_row = (y+ancor.y) / 8
 
 	return mget(
 		player_column,
@@ -47,39 +35,43 @@ function get_sprite(x,y)
 	)
 end
 
--- checks if a sprite at (x,y) position has the desired flag
-function check_flag(x,y,f)
-	return fget(get_sprite(x,y), f)
+-- returns if a sprite at (x,y) position has the desired flag
+function has_flag(x, y, f)
+	return fget(get_sprite(x, y), f)
 end
 
-function check_plate(x,y)
-	if check_flag(x, y, 1) then
-		checkmarks = checkmarks + 1
+-- check if a plate is pressed
+function check_plate(x, y)
+	local sprite = get_sprite(x, y)
+	if sprite >= 96 and sprite <= 99 then
 		mset(
 			(x+ancor.x) / 8,
 			(y+ancor.y) / 8,
-			get_sprite(x,y)+1
+			100
 		)
-		if checkmarks == 4 then
-			unlock_doors()
-		end
+		unlock_door(sprite - 96)
 	end
 end
 
--- unlocks all doors on the map
-function unlock_doors()
-	for _, door in ipairs(doors) do
-		for _, pos in ipairs(door) do
-		sprite = mget(
-			pos.x,
-			pos.y
-		)
+-- unlocks the doors on the map. 𝘵his starts at the 
+function unlock_door(d)
+	local door = doors[d]
+	local door_x = door.x
+	local door_y = door.y
+	local sprite = mget(door.x, door.y)
+
+	-- repeat until sprite has not the door flag
+	while fget(sprite, flag_door) do
 		mset(
-			pos.x,
-			pos.y,
+			door_x,
+			door_y,
 			sprite - 48
 		)
-		end
+		door_y = door_y + door.d
+		sprite = mget(
+			door_x,
+			door_y
+		)
 	end
 end
 
@@ -92,30 +84,28 @@ function handle_input()
 	-- check buttons
 	local dx = 0
 	local dy = 0
-	if (btn(0)) then dx=dx-1 end
-	if (btn(1)) then dx=dx+1 end
-	if (btn(2)) then dy=dy-1 end
-	if (btn(3)) then dy=dy+1 end
+	if btn(0) then dx=dx-1 end
+	if btn(1) then dx=dx+1 end
+	if btn(2) then dy=dy-1 end
+	if btn(3) then dy=dy+1 end
 
 	-- next position
 	local dis = dx * dx + dy * dy
-	local px = player.x + (dis and dx * sqrt_2 or dx)
-	local py = player.y + (dis and dy * sqrt_2 or dy)
-	
+	local px = player.x + (dis and dx / sqrt_2 or dx)
+	local py = player.y + (dis and dy / sqrt_2 or dy)
+
 	-- check if it does not hit a wall
-	if check_flag(px, py, 0)
-	then
-		handle_movement(px,py)
-		handle_animation(dx,dy)
+	if has_flag(px, py, flag_free) then
+		handle_movement(px, py)
+		handle_animation(dx, dy)
 	end
 end
-
 
 function handle_movement(px, py)
 	player.x = px
 	player.y = py
-	
-	check_plate(px, py)	
+
+	check_plate(px, py)
 end
 
 function handle_animation(dx, dy)
@@ -124,7 +114,7 @@ function handle_animation(dx, dy)
 	elseif dx > 0 then animation=2
 	elseif dy < 0 then animation=3
 	end
-	player.animation = ani
+	player.animation = animation
 end
 
 function _update()
@@ -133,7 +123,6 @@ end
 
 function _draw()
 	cls(5)
-	map(0,0,-player.x,-player.y,16,16)
-	print(checkmarks .. " / 4")
-	spr(player.animation,ancor.x-4,ancor.y-8)
+	map(0, 0, -player.x,-player.y, 16, 16)
+	spr(player.animation, ancor.x-4, ancor.y-8)
 end
